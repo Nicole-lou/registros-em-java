759faffd511a6ddf05005a9183d8b88096bbeff1
Nicole Lourenço <nicolelourenco38@gmail.com>
2026-02-20 15:38:14 -0300

    armazenamento de dados

---
+++ src/employeeRecord.java
@@ -0,0 +1,7 @@
+public record employeeRecord(String name, int employeeNumber) {
+}
+
+
+
+
+/* Modo simples de fazer de armazenamento de dados de pessoas */
--- src/recordsTutorial.java
+++ src/recordsTutorial.java
@@ -2,5 +2,7 @@ public class recordsTutorial {
     // Modo tradicional
     public static void main(String[] args) {
         employeeClass Employee = new employeeClass("Matheus", 12356 );
+
+        employeeRecord EmployeeRecord = new employeeRecord("Karen", 1203);
     }
 }
